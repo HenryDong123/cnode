e4e191db1c99a203a42de0585b9898731468d782
HenryDong123 <943082205@qq.com>
2018-12-12 01:30:46 +0800

    头部组件基本完成，开始postList组件

---
+++ src/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="header">
+    <img src="../assets/cnodejs_light.svg" alt="">
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">注册</a></li>
+      <li><a href="#">登录</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  *::after {
+    box-sizing: border-box;
+  }
+
+  *::before {
+    box-sizing: border-box;
+  }
+
+  ul, ol {
+    list-style: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  input {
+    font: inherit;
+  }
+
+  .header {
+    background: #444444;
+    height: 51px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+  }
+
+  img {
+    max-width: 112px;
+    margin-left: 50px;
+
+  }
+
+  ul {
+
+    margin: 4px;
+  }
+
+  li {
+    display: inline-block;
+    font-size: 13px;
+    padding: 10px 15px;
+  }
+
+  a {
+    color: #ccc;
+
+    line-height: 20px;
+
+  }
+
+
+</style>
---
+++ src/components/postList.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div class="loading" v-if="isLoading">
+
+  </div>
+
+  <div>
+    <ul>
+      <li>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "postList",
+    data(){
+      return{
+        isLoading:false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
